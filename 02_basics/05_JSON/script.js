e0b965222746e682c57b6ec26e4dc5e5a1272d4e
_satya <123727915+opsatya@users.noreply.github.com>
2024-05-16 11:22:19 +0000

    JSON in js

---
+++ 02_basics/05_JSON/script.js
@@ -0,0 +1,32 @@
+    const Course = {
+        CourseName: "JS in Hindi",
+        price : 999,
+        CourseInstructor : "hitesh Sir"
+        
+    }
+    //Course.CourseInstructor   
+    // jab hame code aur thoda clean banana hota hai tab ham is synax ka use karte hai 
+    const {CourseInstructor : instructor} = Course    // object destrctureing 
+  ///  console.log(CourseInstructor);
+    console.log(instructor);
+/*
+this concept os known as destrctuctureing of object in js
+
+api se value JSON form me ati hai pahle XMl form me ati thi      json =(javascript object notation)
+*/
+// {
+//     "name": "satya",
+//     "Age": 19
+//     "Email" : "satya@gmail.com"   // object Form 
+// }
+[
+    {
+
+    },
+    {
+
+    },
+    {
+
+    }
+]
