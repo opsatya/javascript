d2d0e098440a1e97261d0768438d60e84f0e3515
_satya <123727915+opsatya@users.noreply.github.com>
2024-05-18 17:41:19 +0000

    for loop in js

---
+++ 05_loops/for-loop/script.js
@@ -0,0 +1,43 @@
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if(i == 5){
+       // console.log("5 is the best number");
+    }
+    //console.log(i);   
+}
+//console.log(i); // out of the scope 
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i}*${j} = ${i*j}`);
+//     }
+    
+// }
+
+
+// let myarray = ["superman","batman","flash","batman"]
+// for (let index = 0; index < myarray.length; index++) {
+//     const element = myarray[index]
+//     console.log(element);
+    
+// }
+
+
+//break and continue
+// for (let i = 1; i <= 10; i++) {
+//  if(i==5){
+//     console.log(`${i} five dected`);
+//     break;  // breaking the statement iske bad koi bhi statemnt excute nahi hoga
+//  }
+//   console.log(i);  
+//  }
+
+// for (let i = 1; i <= 10; i++) {
+//  if(i==5){
+//     console.log(`five dected`);
+//     continue;    // skip the current iteration
+//  }
+//   console.log(i);  
+// }
+
