0798e9ff3d2712d8570cd9f33640a7b52c2b1d66
_satya <123727915+opsatya@users.noreply.github.com>
2024-05-19 02:57:31 +0000

    while loop in js

--- 05_loops/while-do-while/script.js
+++ 05_loops/while-do-while/script.js
@@ -1,1 +1,19 @@
-console.log("heello world");
+let index = 1;
+while(index <=10){
+   // console.log(`value is ${index}`);
+    index = index+1
+}
+
+let myarr = ["flash","batman","superman"]
+let arr = 0
+while(arr<myarr.length){
+   // console.log(myarr[arr]);
+    arr++
+}
+
+
+let score = 11
+do {
+    console.log(`value is ${score}`);
+    score++
+} while (score<= 10);
