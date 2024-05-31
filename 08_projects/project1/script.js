c75527f52a25e5773750f282d9328a58e0c2a5ac
_satya <123727915+opsatya@users.noreply.github.com>
2024-05-31 07:40:42 +0000

    color switcher project

--- 08_projects/project1/script.js
+++ 08_projects/project1/script.js
@@ -8,18 +8,18 @@ buttons.forEach((button) => {
         console.log(event);
         console.log(event.target);
 
-        // if (event.target.id === 'grey') {
-        //     body.style.backgroundColor = 'gray';
-        // }
-        //  else if (event.target.id === 'white') {
-        //      body.style.backgroundColor = event.target.id;
-        //  }
-        //  else if (event.target.id === 'blue') {
-        //     body.style.backgroundColor = event.target.id;
-        // }
-        // else if (event.target.id === 'yellow') {
-        //     body.style.backgroundColor = event.target.id;
-        // }
+        if (event.target.id === 'grey') {
+            body.style.backgroundColor = 'gray';
+        }
+         else if (event.target.id === 'white') {
+             body.style.backgroundColor = event.target.id;
+         }
+         else if (event.target.id === 'blue') {
+            body.style.backgroundColor = event.target.id;
+        }
+        else if (event.target.id === 'yellow') {
+            body.style.backgroundColor = event.target.id;
+        }
     });
 });
 
